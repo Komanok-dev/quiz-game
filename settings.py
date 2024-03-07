6ae4fd6d1b944e9a007ed2ae8acf525b708d1672
Komanok-dev <s.komanok@gmail.com>
2024-03-07 15:04:00 +0000

    Initial commit

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ QUESTIONS = 'questions1.txt'
 INTRO = 'intro.mp3'
 SND_PAUSE = [f'pause{num}.mp3' for num in range(1, 5)]
 
-SND_CLOCK = 'clock.mp3'
+SND_CLOCK = 'clock.mp3'     # Clock ticking lasts 2 minutes
 
 TEAM1 = 'Девочки'
 TEAM2 = 'Мальчики'
